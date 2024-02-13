479bec16fb2b64c6e0e31fdbbc5836c6e35207db
Isidora-Djordjevic <doradjordjevic2001@gmail.com>
2024-02-13 20:53:05 +0100

    Napravljen je main layout. Modifikovan App.jsx. Napravljena logout akcija.

---
+++ coin-compass/coin-compass/coin-compass/src/App.jsx
@@ -0,0 +1,26 @@
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+// Rute
+import Dashboard, { dashboardLoader } from "./pages/Dashboard";
+import Error from "./pages/Error";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Dashboard />,
+    loader: dashboardLoader,
+    errorElement: <Error />
+  },
+]);
+
+function App() {
+  return <div className="App">
+    <RouterProvider router={router} />
+  </div>;
+}
+
+export default App;
+
