f9e38e696bb3a41b48d82b48074ae711dab991b5
zdravkovicnikola <nzdravkovic2001@gmail.com>
2024-02-14 22:09:02 +0100

    Obradjena Error stranica

--- coin-compass/src/pages/Error.jsx
+++ coin-compass/src/pages/Error.jsx
@@ -1,8 +1,30 @@
+import React from 'react'
+import { useRouteError, Link, useNavigate } from "react-router-dom"
+
 const Error = () => {
+  const error = useRouteError();
+  const navigate = useNavigate();
   return (
-    <div>
-      Error
+    <div className="error">
+      <h1>Ups! Imamo problem.</h1>
+      <p>{error.message || error.statusText}</p>
+      <div className="flex-md">
+        <button
+          className="btn btn--dark"
+          onClick={() => navigate(-1)}
+        >
+          
+          <span>Nazad</span>
+        </button>
+        <Link
+          to="/"
+          className="btn btn--dark"
+        >
+          <span>Pocetna strana</span>
+        </Link>
+      </div>
     </div>
   )
 }
+
 export default Error
